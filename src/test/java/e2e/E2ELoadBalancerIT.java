16b2fc291c02af5a9e9a9155b7ddd893b7fd1ec7
Hwaai Kang Kee <hwaaikang@gmail.com>
2024-03-10 04:43:52 +0800

    1. Moved integration tests to separate e2e folder following Maven standard project layout 2. Renamed existing success scenario integration test to all servers healthy scenario 3. Added scenario for integration test where all servers fail 4. Added application-integrationtest.properties file for integration test config

--- src/test/java/e2e/E2ELoadBalancerIT.java
+++ src/test/java/e2e/E2ELoadBalancerIT.java
@@ -1,3 +1,5 @@
+package e2e;
+
 import com.example.echoapi.EchoAPIApplication;
 import com.example.loadbalancer.LoadBalancerApplication;
 import org.junit.jupiter.api.Test;
@@ -7,12 +9,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 @SpringBootTest(classes = LoadBalancerApplication.class)
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class E2ELoadBalancerIT {
 
     private static final String PATH = "/api/roundrobin";
@@ -21,15 +28,14 @@ public class E2ELoadBalancerIT {
     private MockMvc mockMvc;
 
     @Test
-    public void testRoundRobinEndpoint_Success() throws Exception {
-        // Start the additional Spring Boot application programmatically
+    public void testRoundRobinEndpoint_AllServersHealthy() throws Exception {
         final ConfigurableApplicationContext server1 = new SpringApplicationBuilder()
                 .sources(EchoAPIApplication.class)
-                .properties("server.port=8080") // Set a random port for the additional application
+                .properties("server.port=8080")
                 .run();
         final ConfigurableApplicationContext server2 = new SpringApplicationBuilder()
                 .sources(EchoAPIApplication.class)
-                .properties("server.port=8081") // Set a random port for the additional application
+                .properties("server.port=8081")
                 .run();
 
         final String requestBody = "{\"key\": \"value\"}";
@@ -42,5 +48,16 @@ public class E2ELoadBalancerIT {
 
         server1.close();
         server2.close();
+    }
+
+    @Test
+    public void testRoundRobinEndpoint_AllServersFail() throws Exception {
+        final String requestBody = "{\"key\": \"value\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post(PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isServiceUnavailable())
+                .andExpect(res -> assertInstanceOf(ResponseStatusException.class, res.getResolvedException()));
     }
 }

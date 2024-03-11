ae8173ab8d1917c5058c38c7c6d7443aa9528a33
Hwaai Kang Kee <hwaaikang@gmail.com>
2024-03-11 22:09:20 +0800

    added javadoc for LoadBalancerServiceBase

--- src/main/java/com/example/loadbalancer/service/LoadBalancerServiceBase.java
+++ src/main/java/com/example/loadbalancer/service/LoadBalancerServiceBase.java
@@ -8,10 +8,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
+/**
+ * Base class for LoadBalancerService implementations.
+ */
 abstract class LoadBalancerServiceBase implements LoadBalancerService {
     private static final Logger LOG = Logger.getLogger(LoadBalancerServiceBase.class.getName());
 
     private final RestClient restClient;
+
     final List<String> uris;
 
     public LoadBalancerServiceBase(final RestClient restClient, final List<String> uris) {
@@ -19,6 +23,13 @@ abstract class LoadBalancerServiceBase implements LoadBalancerService {
         this.uris = uris;
     }
 
+    /**
+     * Sends the request to the given server with request body
+     *
+     * @param uri         URI of underlying server to send request to
+     * @param requestBody request body
+     * @return response body
+     */
     Map<String, Object> processRequest(final String uri, final Map<String, Object> requestBody) {
         LOG.info("Server:%s processing request with body %s".formatted(uri, requestBody));
         return restClient.post()

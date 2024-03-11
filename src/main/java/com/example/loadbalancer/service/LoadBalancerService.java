c7df8b531b321841d1aa46744d63939cf1ebd948
Hwaai Kang Kee <hwaaikang@gmail.com>
2024-03-11 22:05:30 +0800

    added javadoc for LoadBalancerService

--- src/main/java/com/example/loadbalancer/service/LoadBalancerService.java
+++ src/main/java/com/example/loadbalancer/service/LoadBalancerService.java
@@ -2,6 +2,16 @@ package com.example.loadbalancer.service;
 
 import java.util.Map;
 
-public interface LoadBalancerService {
+/**
+ * Load balancer service which processes incoming requests
+ */
+interface LoadBalancerService {
+
+    /**
+     * Processes incoming request for the load balancer
+     *
+     * @param requestBody request body of incoming request
+     * @return response body
+     */
     Map<String, Object> processRequest(final Map<String, Object> requestBody);
 }
